8c9cf4f65811265cbd266a05ad5aa51e98be1a67
Michał Lipski <michal.lipski@snowflake.com>
2023-03-07 14:14:38 +0100

    solution

--- src/proxy/consoleWithTimestamp.ts
+++ src/proxy/consoleWithTimestamp.ts
@@ -1,2 +1,13 @@
 
-export const consoleWithTimestamp = console;
+const logMethods: (string | symbol)[]= ['log', 'debug', 'info', 'error'];
+
+export const consoleWithTimestamp = new Proxy(console, {
+  get: (target, prop) => {
+    if (!logMethods.includes(prop)) return target[prop];
+
+    return (...args) => {
+      const timestamp = new Date().toLocaleString();
+      target[prop](timestamp, ...args);
+    }
+  }
+});
