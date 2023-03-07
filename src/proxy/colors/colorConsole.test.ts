473374e2b13c3ac6f5dfb923ea4e47cf52405900
Michał Lipski <michal.lipski@snowflake.com>
2023-03-07 14:30:38 +0100

    color excercise solution

--- src/proxy/colors/colorConsole.test.ts
+++ src/proxy/colors/colorConsole.test.ts
@@ -2,5 +2,8 @@ import { colorConsole } from './colorConsole';
 
 describe('this', () => {
   it('should work', () => {
+    colorConsole(console).red('Hello')
+    colorConsole(console).green('Hello')
+    colorConsole(console).blue('Hello')
   });
 });
--- src/proxy/colors/colorConsole.ts
+++ src/proxy/colors/colorConsole.ts
@@ -2,5 +2,10 @@ const BLUE = `\x1b[34m%s\x1b[0m`;
 const RED = `\x1b[31m%s\x1b[0m`;
 const GREEN = `\x1b[32m%s\x1b[0m`;
 
-export const colorConsole = { }
+export const colorConsole = (console) => {
+  console.red = msg => console.log(RED, msg);
+  console.green = msg => console.log(GREEN, msg);
+  console.blue = msg => console.log(BLUE, msg);
+  return console;
+};
 
