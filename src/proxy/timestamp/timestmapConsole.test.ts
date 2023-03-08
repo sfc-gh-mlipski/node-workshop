8162206c883334ea611c8801fdd97a3da403e957
Mikołaj Piekutowski <mikolaj.piekutowski@snowflake.com>
2023-03-08 13:52:39 +0100

    Proxy

--- src/proxy/timestamp/timestmapConsole.test.ts
+++ src/proxy/timestamp/timestmapConsole.test.ts
@@ -1,8 +1,27 @@
 import { timestampConsole } from './timestmapConsole';
 
-describe('this', () => {
-  it('should work', () => {
-    timestampConsole.log('Hello')
-    timestampConsole.log('Hello','World')
+const now = new Date('08.03.2023');
+
+jest.useFakeTimers();
+jest.setSystemTime(now);
+
+const spy = jest.spyOn(console, 'log');
+
+describe('console', () => {
+  it('log should prepend date', () => {
+    const res = timestampConsole.log('Hello');
+    expect(spy).toBeCalledWith(`${now} Hello`);
   });
+  // it('info should prepend date', () => {
+  //   const res = timestampConsole.info('Hello');
+  //   expect(res).toEqual('08.03.2023 Hello');
+  // });
+  // it('error should prepend date', () => {
+  //   const res = timestampConsole.error('Hello');
+  //   expect(res).toEqual('08.03.2023 Hello');
+  // });
+  // it('debug should prepend date', () => {
+  //   const res = timestampConsole.debug('Hello');
+  //   expect(res).toEqual('08.03.2023 Hello');
+  // });
 });
--- src/proxy/timestamp/timestmapConsole.ts
+++ src/proxy/timestamp/timestmapConsole.ts
@@ -1,2 +1,12 @@
+'no strict';
 
-export const timestampConsole = console;
+export const timestampConsole = new Proxy(console, {
+  get: (target, prop, foo) => {
+    if (!['log', 'info', 'debug', 'error'].includes(prop as string)) return target[prop];
+
+    return args => target[prop](new Date(), args);
+  },
+});
+
+timestampConsole.log('hi');
+timestampConsole.table([1, 2, 3]);
